d32d8b94322749ca66ce627f4e15ca3f70ab859b
Jessie Kong <jessiekong@Jessies-MacBook-Air-8.local>
2026-02-20 01:39:40 -0800

    Initial Flask + Postgres + Gunicorn + Nginx setup

---
+++ web/project/models.py
@@ -0,0 +1,13 @@
+from project import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
+
